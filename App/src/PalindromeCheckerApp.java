508abc3232a5ec4ed49df50ddc5713996d2c9ae0
dheerajsai18 <dheerajsai833@gmail.com>
2026-02-25 16:11:41 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,5 @@
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 public class PalindromeCheckerApp {
     public static void main(String[]args){
         // Declare and initialize the input string
